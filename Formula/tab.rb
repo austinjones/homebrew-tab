98aaaed5564c75c552894726d5182354e231cabe
Austin Jones <implAustin@gmail.com>
2020-09-13 12:49:48 -0400

    Update tab.rb

--- Formula/tab.rb
+++ Formula/tab.rb
@@ -4,7 +4,7 @@ class Tab < Formula
   url "https://github.com/austinjones/tab-rs/archive/v0.3.2.tar.gz"
   sha256 "4b988a23189213ed4826236faee2c9b893ba947a062ce92e965b43d78bae7c88"
   license "MIT"
-  revision 35
+  revision 36
   head "https://github.com/austinjones/tab-rs.git"
 
   bottle do
