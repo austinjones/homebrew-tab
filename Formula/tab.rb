9c997805bac40adedc40778bb39d2335e3a51495
Austin Jones <implAustin@gmail.com>
2020-09-12 23:18:53 -0400

    Update description

--- Formula/tab.rb
+++ Formula/tab.rb
@@ -1,5 +1,5 @@
 class Tab < Formula
-  desc "A modern terminial multiplexer designed for software & systems engineers"
+  desc "The intuitive, config-driven terminal multiplexer designed for software & systems engineers"
   homepage "https://github.com/austinjones/tab-rs"
   url "https://github.com/austinjones/tab-rs/archive/v0.3.2.tar.gz"
   sha256 "4b988a23189213ed4826236faee2c9b893ba947a062ce92e965b43d78bae7c88"
